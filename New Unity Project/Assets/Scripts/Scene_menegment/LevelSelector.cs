7ebf592c36baaa8db91d8f373217125f5a777136
Titmf <70275472+Titmf@users.noreply.github.com>
2022-10-16 14:49:45 +0400

    TUB_1_Main_Menu
    Create a default main menu

--- New Unity Project/Assets/Scripts/Scene_menegment/LevelSelector.cs
+++ New Unity Project/Assets/Scripts/Scene_menegment/LevelSelector.cs
@@ -1,16 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
+    public int level;
+    public Text levelText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelText.text = level.ToString();
     }
     
     public void OpenScene()
     {
-        SceneManager.LoadScene("Level_1");
+        SceneManager.LoadScene("Level " + level.ToString());
     }
 }
