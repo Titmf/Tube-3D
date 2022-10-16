6b81847a915bcc6d25e0a5a1775910fcab33f13c
Titmf <70275472+Titmf@users.noreply.github.com>
2022-10-16 12:00:22 +0400

    export default settings from my old project

---
+++ New Unity Project/Assets/Scripts/Scene_menegment/LevelSelector.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+}
